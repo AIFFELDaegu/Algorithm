5f1dee4892b91a74aef398f8a93e2ce8ca204c08
“nidolight” <nidolight@outlook.com>
2022-01-17 20:31:18 +0900

    2022_01_nidolight

--- 2022_01_nidolight/5.py
+++ 2022_01_nidolight/5.py
@@ -4,11 +4,27 @@ def compress(s,size):
     count = 1
     skip = 0
 
-    for j in range(len(s)):
-        print(j)
-        pass
+    for k in range(len(s)):
+        if tmp != s[k+size:k+2*size]:
+                tmp = s[k:k+size]
+                
+        if skip == 0:
+            if tmp == s[k+size:k+2*size]:
+                count += 1
+                result += str(count) + tmp
+                skip = size*count-1
+            else:
+                result += s[k]
+            # print(k, tmp, s[k+size:k+2*size], skip,result)
+                            
+        else:
+            count = 1
+            skip -= 1
+            tmp = s[k:k+size]
+            # print(k, tmp,s[k+size:k+2*size],skip,result,'skip')
+            continue
 
-    return len(result)
+    return result
 
 
 def solution(s):
