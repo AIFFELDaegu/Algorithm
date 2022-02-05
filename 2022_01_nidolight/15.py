c7d98f056a8eb884001f591a5fb92beb5ce3844c
“nidolight” <nidolight@outlook.com>
2022-02-06 03:50:07 +0900

    added

--- 2022_01_nidolight/15.py
+++ 2022_01_nidolight/15.py
@@ -1,28 +1,3 @@
-
-def solution(phone_book):
-    dict = Trie()
-    
-    for num in phone_book:
-        dict.insert(num)
-
-    currentDepth = 0
-    maxDepth = dict.maxDepth
-    answer = recursive(dict.root, currentDepth, maxDepth)
-
-    return answer
-
-def recursive(dict, currentDepth, maxDepth):
-    for k in dict:
-        print(currentDepth, maxDepth, dict[k])
-
-        if k == '*':
-            if currentDepth < maxDepth:
-                return False
-            else:
-                return True
-            
-        return recursive(dict[k], currentDepth+1, maxDepth)
-
 class Trie:
     def __init__(self):
         self.root = {}
@@ -33,13 +8,38 @@ class Trie:
         depth = len(s)
 
         if depth>self.maxDepth:
-            self.maxDepth= depth
+            self.maxDepth = depth
 
         for c in s:
             if c not in cur_node:
                 cur_node[c] = {}
             cur_node = cur_node[c]
-        cur_node["*"] = s
+        cur_node['*'] = s
+
+def solution(phone_book):
+    dict = Trie()
+    
+    for num in phone_book:
+        dict.insert(num)
+
+    print(dict.root)
+
+    currentDepth = 0
+    maxDepth = dict.maxDepth
+    answer = recursive(dict.root,currentDepth,maxDepth)
+
+    return answer
+
+def recursive(dict,currentDepth,maxDepth):
+    for k in dict:
+        if k == '*':
+            if currentDepth<maxDepth:
+                return False
+            else:
+                return True
+            
+        return recursive(dict[k], currentDepth+1,maxDepth)
 
 
-print(solution(["0",'1','2']))
+
+print(solution(["123","456","789"]))
