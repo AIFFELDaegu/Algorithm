3e54d3539f4381d1dc8932c9f6e6da42d7b0aa24
“nidolight” <nidolight@outlook.com>
2022-02-07 22:10:08 +0900

    added

--- 2022_01_nidolight/16.py
+++ 2022_01_nidolight/16.py
@@ -13,6 +13,16 @@ def solution(clothes):
 
     return answer - 1
 
+def solution1(clothes):
+    from collections import Counter
+    from functools import reduce
 
-print(solution([["yellowhat", "headgear"], [
+    cnt = Counter([kind for name, kind in clothes])
+    answer = reduce(lambda x, y: x*(y+1), cnt.values(), 1) - 1
+    return answer
+#카운터는 이름과 그 이름의 갯수를 구해줌
+#리듀스는 누적값 계산할 때 유용
+
+print(solution1([["yellowhat", "headgear"], [
       "bluesunglasses", "eyewear"], ["green_turban", "headgear"]]))
+
